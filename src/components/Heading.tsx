988525319b501175e6e00798ec4c314630d50974
Emad <emad90mohamad@gmail.com>
2021-08-14 16:10:49 -0700

    Test

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,10 +1,30 @@
 import * as React from "react"
 
-export const Heading: React.FC<any> = ({
-  size = "5",
+const SIZES = {
+  "1": "text-1",
+  "2": "text-2",
+  "3": "text-3",
+  "4": "text-4",
+  "5": "text-5",
+  "6": "text-6",
+  "7": "text-7",
+  "8": "text-8",
+}
+
+type HeadingProps = {
+  size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+  className?: string
+}
+
+export const Heading: React.FC<HeadingProps> = ({
+  size = 5,
   className = "",
   as: Tag = "h2",
   children,
 }) => {
-  return <Tag className={`text-${size} font-bold`}>{children}</Tag>
+  const headingSize = SIZES[size]
+  const classes = `${className} ${headingSize} font-bold`
+
+  return <Tag className={classes}>{children}</Tag>
 }
